b4743c134458fbb48f1760f0db7fd42e6796fb9f
Bendy Zhang <zb@bndy.net>
2020-02-08 00:31:25 +0800

    refactor: init.ts

--- tools/init.ts
+++ tools/init.ts
@@ -3,64 +3,56 @@ const readline = require('readline');
 const { echo, exec } = require('shelljs');
 const { readFileSync, writeFileSync } = require('fs');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const cli = require('@bndynet/cli');
 
-echo(`
+cli.info(`
 # This utility will walk you through creating a package.json file.
 # It only covers the most common items, and tries to guess sensible defaults.
 
 # Use "npm i"  to install the default packages.
 `);
 
-rl.question('package name: ', (pkgName: string) => {
+cli.questions(['Your package name:', 'Your package description:', 'Author name:', 'Author email:', 'Git repository url:']).then((answers: any[]) => {
+  const pkgName = answers[0];
   const libName = pkgName.indexOf('/') > 0 ? pkgName.split('/')[1].toLowerCase() : pkgName.toLowerCase();
-  rl.question('package description: ', (pkgDescription: string) => {
-    rl.question('author name: ', (username: string) => {
-      rl.question('author email: ', (useremail: string) => {
-        rl.question('git repository url: ', (repoUrl: string) => {
-          const pkg = JSON.parse(readFileSync(path.resolve(__dirname, '..', 'package.json')));
-          pkg.name = pkgName;
-          pkg.description = pkgDescription;
-          pkg.main = `dist/${libName}.umd.js`;
-          pkg.module = `dist/${libName}.es5.js`;
-          if (username) {
-            pkg.author.name = username.trim();
-          }
-          if (useremail) {
-            pkg.author.email = useremail.trim();
-          }
-          pkg.repository.url = repoUrl.trim();
+  const pkgDescription = answers[1].trim();
+  const username = answers[2].trim();
+  const useremail = answers[3].trim();
+  const repoUrl = answers[4].trim();
+  const pkgJson = cli.getPackage('../package.json');
 
-          const pkgContent = JSON.stringify(pkg, null, 2);
-          writeFileSync(path.resolve(__dirname, '..', 'package.json'), pkgContent);
+  pkgJson.name = pkgName;
+  pkgJson.description = pkgDescription;
+  pkgJson.main = `dist/${libName}.umd.js`;
+  pkgJson.module = `dist/${libName}.es5.js`;
+  if (username) {
+    pkgJson.author.name = username;
+  }
+  if (useremail) {
+    pkgJson.author.email = useremail;
+  }
+  pkgJson.repository.url = repoUrl;
 
-          echo('#################################');
-          echo('# Generate package.json:');
-          echo(pkgContent);
-          rl.close();
+  cli.startSection('Generate package.json file');
+  const pkgContent = JSON.stringify(pkgJson, null, 2);
+  writeFileSync(path.resolve(__dirname, '..', 'package.json'), pkgContent);
+  cli.print(pkgContent);
+  cli.success('done', true);
 
-          echo('#################################');
-          echo('# Generate site:');
-          const templateIndexFilePath = path.resolve(__dirname, '../_templates', 'index.html');
-          const indexFilePath = path.resolve(__dirname, '../site', 'index.html');
-          let siteIndexHtml =  readFileSync(templateIndexFilePath, 'utf8');
-          siteIndexHtml = siteIndexHtml.replace(/{{package.name}}/g, pkg.name)
-            .replace(/{{package.subname}}/g, libName)
-            .replace(/{{package.repository.url}}/g, pkg.repository.url);
-          writeFileSync(indexFilePath, siteIndexHtml, (werr: any) => {
-            if (werr) {
-              throw werr;
-            }
-          });
-
-          echo('#################################');
-          echo('# Install dependencies:');
-          exec('npm i');
-        });
-      });
-    });
+  cli.startSection('Generate Site');
+  const templateIndexFilePath = path.resolve(__dirname, '../_templates', 'index.html');
+  const indexFilePath = path.resolve(__dirname, '../site', 'index.html');
+  let siteIndexHtml = readFileSync(templateIndexFilePath, 'utf8');
+  siteIndexHtml = siteIndexHtml
+    .replace(/{{package.name}}/g, pkgJson.name)
+    .replace(/{{package.subname}}/g, libName)
+    .replace(/{{package.repository.url}}/g, pkgJson.repository.url);
+  writeFileSync(indexFilePath, siteIndexHtml, (werr: any) => {
+    if (werr) {
+      throw werr;
+    }
   });
+
+  cli.startSection('Install dependencies');
+  cli.run('npm i');
 });
